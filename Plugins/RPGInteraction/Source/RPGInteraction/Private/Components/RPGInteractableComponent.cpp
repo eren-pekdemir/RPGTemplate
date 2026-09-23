ff486c4d4ebde6c9f48b996ccd8a73f4bee4326a
Eren Pekdemir <eren.pekdemir.15@gmail.com>
2026-09-23 20:48:18 +0300

    feat(interaction): add interactable component

---
+++ Plugins/RPGInteraction/Source/RPGInteraction/Private/Components/RPGInteractableComponent.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/RPGInteractableComponent.h"
+
+
+// Sets default values for this component's properties
+URPGInteractableComponent::URPGInteractableComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+bool URPGInteractableComponent::CanInteract_Implementation(AActor* InteractionInstigator) const
+{
+	return bEnabled;
+}
+
+FText URPGInteractableComponent::GetInteractionPrompt_Implementation(AActor* InteractionInstigator) const
+{
+	return InteractionPrompt;
+}
+
+float URPGInteractableComponent::GetInteractionDuration_Implementation() const
+{
+	return HoldDuration;
+}
+
+void URPGInteractableComponent::Interact_Implementation(AActor* InteractionInstigator)
+{
+	if (IRPGInteractable::Execute_CanInteract(this, InteractionInstigator))
+	{
+		OnInteracted.Broadcast(InteractionInstigator);
+		
+		if (bSingleUse)
+		{
+			SetInteractionEnabled(false);
+		}
+	}
+}
+
+void URPGInteractableComponent::SetInteractionEnabled(bool bNewEnabled)
+{
+	bEnabled = bNewEnabled;
+}
+
---
+++ Plugins/RPGInteraction/Source/RPGInteraction/Public/Components/RPGInteractableComponent.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Interfaces/RPGInteractable.h"
+#include "RPGInteractableComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FRPGOnInteractedSignature, AActor*, InteractionInstigator);
+
+UCLASS(ClassGroup=(RPG), meta=(BlueprintSpawnableComponent))
+class RPGINTERACTION_API URPGInteractableComponent : public UActorComponent , public IRPGInteractable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	URPGInteractableComponent();
+	
+	//Delegate
+	UPROPERTY(BlueprintAssignable, Category = "Interaction")
+	FRPGOnInteractedSignature OnInteracted;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
+	FText InteractionPrompt = NSLOCTEXT("RPGInteraction", "DefaultPrompt", "Interact");
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction", meta = (ClampMin = "0.0", Units = "s"))
+	float HoldDuration = 0.f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction")
+	bool bEnabled = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
+	bool bSingleUse = false;
+	
+	//Interface Functions
+	virtual bool CanInteract_Implementation(AActor* InteractionInstigator) const override;
+	virtual FText GetInteractionPrompt_Implementation(AActor* InteractionInstigator) const override;
+	virtual float GetInteractionDuration_Implementation() const override;
+	virtual void Interact_Implementation(AActor* InteractionInstigator)  override;
+	
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	void SetInteractionEnabled(bool bNewEnabled);
+}
