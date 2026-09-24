99122381db55d5888b48a4a1842b717f084c69fa
Eren Pekdemir <eren.pekdemir.15@gmail.com>
2026-09-24 19:04:59 +0300

    feat(interaction): add interactor component with focus scanning and test setup

---
+++ Plugins/RPGInteraction/Source/RPGInteraction/Private/Components/RPGInteractorComponent.cpp
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/RPGInteractorComponent.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/Controller.h"
+#include "TimerManager.h"
+#include "Interfaces/RPGInteractable.h"
+
+
+// Sets default values for this component's properties
+URPGInteractorComponent::URPGInteractorComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+// Called when the game starts
+void URPGInteractorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(ScanTimerHandle,this, &ThisClass::ScanForInteractables, ScanInterval, true);
+	
+}
+
+void URPGInteractorComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	GetWorld()->GetTimerManager().ClearTimer(ScanTimerHandle);
+	Super::EndPlay(EndPlayReason);
+}
+
+void URPGInteractorComponent::ScanForInteractables()
+{
+	APawn* Pawn = Cast<APawn>(GetOwner());
+	if (!Pawn || !Pawn->IsLocallyControlled()|| !Pawn->GetController()) return;
+	
+	if (!Detector) return;
+	
+	FRPGInteractionQuery Query;
+	Query.InstigatorActor = Pawn;
+	Pawn->GetController()->GetPlayerViewPoint(Query.ViewLocation, Query.ViewRotation);
+	
+	UObject* NewFocus = Detector->FindBestInteractable(Query);
+	
+	if ( NewFocus != FocusedInteractable.Get())
+	{
+		SetFocus(NewFocus);
+	}
+}
+
+void URPGInteractorComponent::SetFocus(UObject* NewFocus)
+{
+	UObject* OldFocus= FocusedInteractable.Get();
+	FocusedInteractable = NewFocus;
+	OnFocusChanged.Broadcast(NewFocus, OldFocus);
+}
+
+void URPGInteractorComponent::StartInteraction()
+{
+	UObject* Focus = FocusedInteractable.Get();
+	if (!Focus) return;
+	
+	if(!IRPGInteractable::Execute_CanInteract(Focus,GetOwner())) return;
+	
+	IRPGInteractable::Execute_Interact(Focus,GetOwner());
+		
+}
+
+void URPGInteractorComponent::StopInteraction()
+{
+}
---
+++ Plugins/RPGInteraction/Source/RPGInteraction/Public/Components/RPGInteractorComponent.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Detectors/RPGInteractionDetector.h"
+#include "RPGInteractorComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FRPGOnFocusChangedSignature, UObject*, NewFocus, UObject*, OldFocus);
+
+UCLASS(ClassGroup=(RPG), meta=(BlueprintSpawnableComponent))
+class RPGINTERACTION_API URPGInteractorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	URPGInteractorComponent();
+	
+	UPROPERTY(EditAnywhere, Instanced, Category = "Interaction")
+	TObjectPtr<URPGInteractionDetector> Detector;
+	
+	UPROPERTY(EditAnywhere, Category = "Interaction",meta = (ClampMin = "0.01", Units = "s"))
+	float  ScanInterval = 0.1f;
+	
+	UPROPERTY(BlueprintAssignable, Category = "Interaction")
+	FRPGOnFocusChangedSignature OnFocusChanged;
+	
+	UFUNCTION(BlueprintCallable, Category="Interaction")
+	void StartInteraction();
+	
+	UFUNCTION(BlueprintCallable, Category="Interaction")
+	void StopInteraction();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	
+	TWeakObjectPtr<UObject> FocusedInteractable;
+	
+	FTimerHandle ScanTimerHandle;
+	
+	void ScanForInteractables();
+	
+	void SetFocus(UObject* NewFocus);
+};
